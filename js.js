e61cb81d40e7df33134e5dd16ee6246208483d85
Tatyana <ta_ledkova@bk.ru>
2019-02-07 20:51:52 +0400

    second commit

---
+++ js.js
@@ -0,0 +1,7 @@
+
+let age = document.getElementById('age');
+let user = {
+	name: 'John',
+	surname:'Smith'
+};
+alert("Пользователь " + user.surname + " " + user.name + ", его возраст " + age.value);
